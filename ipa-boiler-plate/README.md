305f50d1860ec1423c20caf552fa2c611487d697
bwesterg <benton.westergaard@gmail.com>
2021-07-08 14:11:21 -0600

    First commit, npm install.

---
+++ ipa-boiler-plate/README.md
@@ -0,0 +1,7 @@
+# IPA APP 
+
+Clone this repo and `npm install`
+
+To run the json server run `json-server --watch db.json` 
+
+To start the react server run `npm start` 
---
+++ ipa-boiler-plate/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+// to run the json server run => json-server --watch db.json
+
+//testing 123 to ensure github repository is working correctly. -BW
+
+//final test for github
+
+function App() {
+  return (
+    <div className="App">
+      <h1>IPA APP</h1>
+      <h2>Who needs API's when you have IPA's</h2>
+    </div>
+  );
+}
+
+export default App;
