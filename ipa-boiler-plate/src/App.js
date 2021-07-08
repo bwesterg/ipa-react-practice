6c35499561dcd51e5a948b4b20628f0c22bca9e2
bwesterg <benton.westergaard@gmail.com>
2021-07-08 14:20:03 -0600

    more github problems troubleshooting.

--- ipa-boiler-plate/src/App.js
+++ ipa-boiler-plate/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 
 //Trying to figure out github stuff, ensure files are merging.
 
+// //I don't understand what is happening.  Keep getting message "error: Your local changes to the following files would be overwritten by checkout:
+//         .DS_Store
+//         Please commit your changes or stash them before you switch branches.
+//         Aborting"
+
 function App() {
   return (
     <div className="App">
